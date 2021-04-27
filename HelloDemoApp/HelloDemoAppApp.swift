83a0155735399a246e6d053ecb0d129e23db36b8
Martin Sheng <martin_sheng@hotmail.com>
2021-04-27 09:57:32 +0200

    Add Project

---
+++ HelloDemoApp/HelloDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloDemoAppApp.swift
+//  HelloDemoApp
+//
+//  Created by Yuangang Sheng on 2021/4/27.
+//
+
+import SwiftUI
+
+@main
+struct HelloDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
